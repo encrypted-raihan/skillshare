e2c7a19c2458ae5b92eee03d76db738dd71e7b91
Raihan Shiras <raihanshiras08@gmail.com>
2026-09-07 11:27:42 +0530

    fix: show mobile bottom nav on explore

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,5 +1,11 @@
 import { ExploreDashboard } from '@/components/explore-dashboard'
+import { MobileBottomNav } from '@/components/mobile-bottom-nav'
 
 export default function ExplorePage() {
-  return <ExploreDashboard />
+  return (
+    <>
+      <ExploreDashboard />
+      <MobileBottomNav />
+    </>
+  )
 }
